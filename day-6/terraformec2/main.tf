d3adc0df64aff85da94583c5dd62867c6cd1a091
Kumar <kumara41@legal.regn.net>
2025-07-23 12:18:18 +0530

    jenkins changes

--- day-6/terraformec2/main.tf
+++ day-6/terraformec2/main.tf
@@ -7,5 +7,6 @@ resource "aws_instance" "test" {
     instance_type = var.instance_type_val
     vpc_security_group_ids = [var.sg_val]
     key_name = "binary"
+    subnet_id = var.subnet_id
 
 }
--- day-6/terraformec2/variable.tf
+++ day-6/terraformec2/variable.tf
@@ -10,3 +10,8 @@ variable "instance_type_val" {
 variable "sg_val" {
   description = "use for security group value"
 }
+
+variable "subnet_id" {
+  description = "used for the subnet"
+  default = "subnet-0890f67a5b68c7c02"
+}
