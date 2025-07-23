fb74006955d7e9cf536c3b24d13f6c9668a2b6c4
Kumar <kumara41@legal.regn.net>
2025-07-23 09:42:24 +0530

    jenkins with terraform on day-6

---
+++ day-6/terraformec2/main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "test" {
+    ami = var.ami_val
+    instance_type = var.instance_type_val
+    vpc_security_group_ids = [var.sg_val]
+    key_name = "binary"
+
+}
---
+++ day-6/terraformec2/variable.tf
@@ -0,0 +1,12 @@
+variable "ami_val" {
+    description = "use for the ami value"
+}
+
+variable "instance_type_val" {
+    description = "use for instance type value"
+  
+}
+
+variable "sg_val" {
+  description = "use for security group value"
+}
